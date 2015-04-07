f68f5bbfc397b5048a5cf67d52dab0f231142f29
Athan Clark <athan.clark@gmail.com>
2015-04-07 00:16:12 -0600

    should have proper nesting

--- src/Web/Routes/Nested.hs
+++ src/Web/Routes/Nested.hs
@@ -29,11 +29,13 @@ import           Control.Monad.Reader
 import qualified Data.List.NonEmpty                as NE
 import           Data.Monoid
 import           Data.Trie.Pseudo
+import qualified Data.Trie.Pseudo                  as P
 import           Data.Trie.Rooted
 import qualified Data.Trie.Rooted                  as R
 import           Data.Traversable
 import qualified Data.Text                         as T
 import qualified Data.Map.Lazy                     as M
+import qualified Data.ByteString.Lazy              as BL
 
 
 newtype HandlerT z m a = HandlerT
@@ -52,13 +54,24 @@ handle :: Monad m =>
        -> VerbListenerT z Response m ()
        -> [HandlerT z m ()]
        -> HandlerT z m ()
-handle ts vl childs = do
+handle ts vl [] = do
   vfrs <- lift $ execWriterT $ runVerbListenerT vl
 
   HandlerT $ tell $
     case ts of
       [] -> MergeRooted $ Rooted (Just vfrs) []
       _  -> MergeRooted $ Rooted Nothing [Rest (NE.fromList ts) vfrs]
+handle ts vl cs = do
+  vfrs <- lift $ execWriterT $ runVerbListenerT vl
+  child <- lift $ foldM (\acc c -> (acc <>) <$> (execWriterT $ runHandler c)) mempty cs
+
+  HandlerT $ tell $
+    case ts of
+      [] -> case unMergeRooted child of
+              Rooted _ xs -> MergeRooted $ Rooted (Just vfrs) xs
+      _  -> let child' = push (unMergeRooted child) $ NE.fromList ts in
+            MergeRooted $ Rooted Nothing [P.assign (NE.fromList ts) (Just vfrs) child']
+
 
 route :: (Functor m, Monad m, MonadIO m) =>
          HandlerT z m a
